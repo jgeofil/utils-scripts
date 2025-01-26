fac378739c35fc2965c84f15a420d931397bdabc
jgeofil <jeremy.geo@gmail.com>
2025-01-25 19:45:03 -0500

    Add initial project setup with .gitignore, README, package.json, TypeScript configuration, and iconify script

---
+++ src/iconify.ts
@@ -0,0 +1,46 @@
+import { readFile } from 'node:fs/promises';
+import { downloadNPMPackage, IconSet, exportToDirectory } from '@iconify/tools';
+
+function getIconSet(prefix) {// Directories
+    const cacheDir = 'cache';
+    const outDir = 'svg';
+
+    // Download all icon sets
+    console.log('Downloading latest package');
+    const downloaded = await downloadNPMPackage({
+        package: '@iconify/json',
+        target: 'node_modules/@iconify/json',
+    });
+    console.log('Downloaded version', downloaded.version);
+
+    // Get a list of icon sets
+    const list = JSON.parse(
+        await readFile(downloaded.contentsDir + '/collections.json', 'utf8')
+    );
+    const prefixes = Object.keys(list);
+    console.log('Got', prefixes.length, 'icon sets');
+
+    // Export each icon set
+    for (let i = 0; i < prefixes.length; i++) {
+        const prefix = prefixes[i];
+
+        // Read file
+        const data = JSON.parse(
+            await readFile(
+                downloaded.contentsDir + '/json/' + prefix + '.json',
+                'utf8'
+            )
+        );
+
+        // Create IconSet
+        const iconSet = new IconSet(data);
+
+        // Export it
+        console.log('Exporting', iconSet.info.name);
+        await exportToDirectory(iconSet, {
+            target: outDir + '/' + prefix,
+        });
+    }
+}
+
+console.log('Done');
